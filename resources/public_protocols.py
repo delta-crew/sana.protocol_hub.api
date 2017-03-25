d86367546e8f5c224ea57b153c1febcae4603711
Jesse Rogers <djrquazz@gmail.com>
2017-03-24 20:41:18 -0400

    Public protocol search

--- resources/public_protocols.py
+++ resources/public_protocols.py
@@ -11,10 +11,14 @@ from schemas import ProtocolSchema
 class PublicProtocolsResource(object):
     def on_get(self, req, resp, organization_id):
         session = req.context['session']
+        query = req.params.get('query', default='')
         protocol_schema = ProtocolSchema()
 
         protocols = session.query(Protocol).\
-                filter(Protocol.public==True).\
+                filter(
+                    Protocol.public==True,
+                    Protocol.title.ilike('%' + query + '%'),
+                ).\
                 all()
 
         data = protocol_schema.dump(protocols)
