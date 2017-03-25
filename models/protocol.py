99b0882cc8aefc7e6411af6c12c92d23408542fd
Jesse Rogers <djrquazz@gmail.com>
2017-03-24 21:20:59 -0400

    Remove description & set autoincrement configuration

--- models/protocol.py
+++ models/protocol.py
@@ -6,9 +6,8 @@ from models.base import Base, PHBareMixin
 class Protocol(PHBareMixin, Base):
     __tablename__ = "ph_protocols"
 
-    id = db.Column(db.Integer, primary_key=True)
-    version = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=False)
+    version = db.Column(db.Integer, primary_key=True, autoincrement=True)
     title = db.Column(db.String)
-    description = db.Column(db.String)
     content = db.Column(db.String)
     public = db.Column(db.Boolean, default=False)
