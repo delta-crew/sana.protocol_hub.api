9d040275ac5328e713731d9fb0f6b663787b66a9
Matt Stobo <mwstobo@gmail.com>
2017-03-25 20:26:14 -0400

    MDS link fixes

--- resources/organization_mds_links.py
+++ resources/organization_mds_links.py
@@ -10,7 +10,7 @@ from schemas import OrganizationMDSLinkSchema
 class OrganizationMDSLinksResource(object):
     @falcon.before(login_required)
     @falcon.before(user_belongs_to_organization)
-    def on_get(self, req, res, organization_id):
+    def on_get(self, req, resp, organization_id):
         organization_mds_link_schema = OrganizationMDSLinkSchema()
         session = req.context['session']
 
@@ -19,44 +19,36 @@ class OrganizationMDSLinksResource(object):
                 filter_by(organization_id=organization_id).\
                 all()
 
-        result = organization_group_member_schema.dump(members)
+        result = organization_mds_link_schema.dump(members)
         resp.context['result'] = result.data
 
     @falcon.before(login_required)
     @falcon.before(authorize_organization_user_to(OrganizationGroup.manage_mds))
-    def on_post(self, req, res, organization_id):
-        organization_group_member_schema = OrganizationMDSLinkSchema()
+    def on_post(self, req, resp, organization_id):
+        organization_mds_link_schema = OrganizationMDSLinkSchema()
         session = req.context['session']
-        user = resp.context['user']
+        user = req.context['user']
 
-        data, errors = organization_mds_link.load(req.context['body'])
+        mds_link, errors = organization_mds_link_schema.load(
+            req.context['body'], session=session)
         if errors:
             resp.stats = falcon.HTTP_BAD_REQUEST
             resp.context['type'] = FAIL_RESPONSE
             resp.context['result'] = errors
             return
 
-        member = session.query(OrganizationMember).\
-                filter_by(
-                    organization_id=organization_id,
-                    id=data['organization_member_id'],
-                ).\
-                one_or_none()
+        mds_link.organization_id = organization_id
 
-        if member == None:
+        organization = session.query(Organization).get(organization_id)
+
+        if organization == None:
             resp.stats = falcon.HTTP_BAD_REQUEST
             resp.context['type'] = FAIL_RESPONSE
-            resp.context['result'] = {'member_id': 'member not found'}
+            resp.context['result'] = {'organization_id': 'organization not found'}
             return
 
-        gmember = OrganizationGroupMember(
-            organization_id=organization_id,
-            organization_member_id=data['organization_member_id'],
-            organization_group_id=group_id,
-        )
-
-        session.add(gmember)
+        session.add(mds_link)
         session.commit()
 
-        result = organization_group_member_schema.dump(organization)
-        resp.conext['result'] = result.data
+        result = organization_mds_link_schema.dump(mds_link)
+        resp.context['result'] = result.data
