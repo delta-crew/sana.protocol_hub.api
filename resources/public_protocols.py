e12baf58cf6a7f76b7c32ce97de60389c18d5ba3
Matt Stobo <mwstobo@gmail.com>
2017-03-27 21:54:02 -0400

    Only return latest version of public protocol

--- resources/public_protocols.py
+++ resources/public_protocols.py
@@ -1,8 +1,9 @@
-from app.hooks import *
 import falcon
+from sqlalchemy.sql import func, label
 
 from app import db
 from app.constants import SUCCESS_RESPONSE, FAIL_RESPONSE
+from app.hooks import *
 from sqlalchemy.orm import joinedload
 from models import Protocol
 from schemas import ProtocolSchema
@@ -14,12 +15,21 @@ class PublicProtocolsResource(object):
         query = req.params.get('query', '')
         protocol_schema = ProtocolSchema(many=True)
 
+        latest_version = session.\
+            query(
+                label('id', Protocol.id),
+                label('max_version', func.max(Protocol.version))).\
+            group_by(Protocol.id).\
+            subquery()
         protocols = session.query(Protocol).\
                 filter(
                     Protocol.public==True,
                     Protocol.title.ilike('%' + query + '%'),
-                ).\
-                all()
+                )
+        most_recent_protocols = protocols.\
+            join(latest_version, Protocol.id==latest_version.c.id).\
+            filter(Protocol.version==latest_version.c.max_version).\
+            all()
 
-        result = protocol_schema.dump(protocols)
+        result = protocol_schema.dump(most_recent_protocols)
         resp.context['result'] = result.data
